0443f9b447da6d554d1709832603170aa14daa8c
khangdev1998 <khangdev171998@gmail.com>
2021-05-04 12:55:10 +0700

    first upload

---
+++ script.js
@@ -0,0 +1,21 @@
+  /* Demo purposes only */
+  $(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+
+// BACK TO TOP SCROLL
+$(document).ready(function(){ 
+  $(window).scroll(function(){ 
+      if ($(this).scrollTop() > 100) { 
+          $('#scroll').fadeIn(); 
+      } else { 
+          $('#scroll').fadeOut(); 
+      } 
+  }); 
+  $('#scroll').click(function(){ 
+      $("html, body").animate({ scrollTop: 0 }, 600); 
+      return false; 
+  }); 
+});
